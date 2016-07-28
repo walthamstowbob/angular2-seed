73a0d458ac3825a4e80411f112afe390eaba5699
Barry O'Brien <barryobrien@MacBook-Pro.local>
2016-07-28 14:02:35 +0200

    new js directory for globs

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@
 
 var gulp = require('gulp'),
     uglify = require('gulp-uglify');
+    compass = require('gulp-compass');
     rename = require('gulp-rename');
 
 // ////////////////////////////////////////////////
@@ -11,10 +12,24 @@ var gulp = require('gulp'),
 // // /////////////////////////////////////////////
 
 gulp.task('scripts', function(){
-    gulp.src(['js/**/*.js', '!js/**/*/.min.js'])
+    gulp.src(['js/**/*.js', '!js/**/*/min.js'])
     .pipe(rename({suffix:'.min'}))
     .pipe(uglify())
-    .pipe(gulp.dest('js'));
+    .pipe(gulp.dest('javascript/js'));
+});
+
+// ////////////////////////////////////////////////
+// Compass / Sass Tasks
+// // /////////////////////////////////////////////
+gulp.task('compass', function(){
+    gulp.src('scss/style.scss')
+        .pipe(compass({
+            config_file:'./config.rb',
+            css:'./css',
+            sass:'./scss',
+            require: ['susy']
+        }))
+        .pipe(gulp.dest('./css'))
 });
 
 // ////////////////////////////////////////////////
@@ -22,9 +37,10 @@ gulp.task('scripts', function(){
 // // /////////////////////////////////////////////
 gulp.task('watch', function(){
     gulp.watch('js/**/*.js', ['scripts']);
+    gulp.watch('scss/**/*.scss', ['compass']);
 });
 
 // ////////////////////////////////////////////////
 // Default Task
 // // /////////////////////////////////////////////
-gulp.task('default', ['scripts', 'watch']);
+gulp.task('default', ['scripts', 'compass', 'watch']);
