be881c1187db384a3bc30f1930b0ac8519d02bd2
Barry O'Brien <barry@leevalleyenergy.co.uk>
2016-08-02 10:32:45 +0200

    lekker footer and css edit

--- app/components/lekker-footer.component.ts
+++ app/components/lekker-footer.component.ts
@@ -14,8 +14,7 @@ import {Component} from 'angular2/core';
                 <li id="inline"><a href="mailto:barry@lekkerit.com" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span></a></li>
                 <li id="inline"><a href="https://twitter.com/lekkerit_" target="_blank" class="btn btn-default btn-lg"><span class="fa fa-twitter" aria-hidden="true"></span></a></li>
                 <li id="inline"><a href="https://nl.linkedin.com/in/lekkerbob" target="_blank" class="btn btn-default btn-lg"><span class="fa fa-linkedin" aria-hidden="true"></span></a></li>
-                
-                        
+                      
             </ul>
             </div>
         </div>
